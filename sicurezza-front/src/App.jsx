3091f4efc877055a505cc845ce49e4a03bc42c90
Jammings <rl.93@hotmail.com>
2022-09-10 00:35:29 +0200

    image to login footer

--- sicurezza-front/src/App.jsx
+++ sicurezza-front/src/App.jsx
@@ -48,8 +48,6 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
-
-      <Footer />
       {/* <button onClick={() => setShowScroll(!showScroll)}>Mostrar Scroll</button>
       {showScroll && <Scroll />} */}
     </div>
