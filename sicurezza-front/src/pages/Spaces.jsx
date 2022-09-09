0180d13ea5ac63ae0dde3588e1e43204087e4778
Jammings <rl.93@hotmail.com>
2022-09-10 00:55:47 +0200

    alt adds

--- sicurezza-front/src/pages/Spaces.jsx
+++ sicurezza-front/src/pages/Spaces.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import Footer from "../components/Footer";
 
 function Spaces() {
   return (
-    <div>
-    </div>
-  )
+    <>
+      <div></div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default Spaces
+export default Spaces;
