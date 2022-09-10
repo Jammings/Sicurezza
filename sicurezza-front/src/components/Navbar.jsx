6acd86c7342f28bdae773d9abb4785031835d459
Carlos Morales <108315881+camogaj@users.noreply.github.com>
2022-09-10 20:58:07 +0200

    icon add

--- sicurezza-front/src/components/Navbar.jsx
+++ sicurezza-front/src/components/Navbar.jsx
@@ -3,92 +3,103 @@ import { logoutUser } from "../redux/auth/auth.actions";
 import { Link, useLocation } from "react-router-dom";
 import "./Navbar.scss";
 import image from "../assets/user.svg";
-
+import arrow from "../assets/arrow.svg"; //añadir iconos
+import icdevice from "../assets/devices.svg";
+import icconfig from "../assets/config.svg";
+import icspaces from "../assets/spaces.svg";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
   const location = useLocation();
   console.log("pathname", location.pathname);
-  
-  return ( true && ( 
-    <div className="navbar">
-      <div className="logo">
-        
+
+  return (
+    true && (
+      <div className="navbar">
+        <div className="logo"></div>
+        {!user && (
+          <Link className={location.pathname === "/" ? "active" : ""} to="/">
+            {/* <span>&nbsp;</span> Home */}
+            <img className="activeicons" src={image} alt="inicio" />{" "}
+            <span>Home</span> {/* //añadir iconos */}
+            <img className="activeicons" src={arrow} alt="inicio" />
+          </Link>
+        )}
+        {!user && (
+          <Link
+            className={location.pathname === "/login" ? "active" : ""}
+            to="/login"
+          >
+            Login
+          </Link>
+        )}{" "}
+        {/*si no existe usuario muestra el link de login*/}
+        {!user && (
+          <Link
+            className={location.pathname === "/register" ? "active" : ""}
+            to="/register"
+          >
+            Register
+          </Link>
+        )}{" "}
+        {/*si no existe usuario muestra el link de register*/}
+        {user && (
+          <Link
+            className={location.pathname === "/start" ? "active" : ""}
+            to="/start"
+          >
+            Inicio
+          </Link>
+        )}
+        {user && (
+          <Link
+            className={location.pathname === "/spaces" ? "active" : ""}
+            to="/spaces"
+          >
+            <img className="activeicons" src={icspaces} alt="inicio" />{" "}
+            <span>Espacios</span>
+            <img className="activeicons" src={arrow} alt="inicio" />
+          </Link>
+        )}
+        {user && (
+          <Link
+            className={location.pathname === "/devices" ? "active" : ""}
+            to="/devices"
+          >
+            <img className="activeicons" src={icdevice} alt="inicio" />{" "}
+            <span>Dispositivos</span>
+            <img className="activeicons" src={arrow} alt="inicio" />
+          </Link>
+        )}
+        {user && (
+          <Link
+            className={location.pathname === "/settings" ? "active" : ""}
+            to="/settings"
+          >
+            <img className="activeicons" src={icconfig} alt="inicio" />{" "}
+            <span>Ajustes</span>
+            <img className="activeicons" src={arrow} alt="inicio" />
+          </Link>
+        )}
+        {user && (
+          <Link
+            className={location.pathname === "/my-account" ? "active" : ""}
+            to="/my-account"
+          >
+            Mi Cuenta
+          </Link>
+        )}
+        {user && (
+          <button className="logout" onClick={() => dispatch(logoutUser())}>
+            Logout
+          </button>
+        )}{" "}
+        {/*si existe usuario muestra el botton de logout*/}
+        {user && <h3>Bienvenido {user.name}</h3>}
       </div>
-      {!user && (
-        <Link className={location.pathname === "/" ? "active" : ""} to="/">
-          {/* <span>&nbsp;</span> Home */}
-          <img className="activeicons" src={image} alt="inicio" /> <span>Home</span>
-        </Link>
-      )}
-      {!user && (
-        <Link
-          className={location.pathname === "/login" ? "active" : ""}
-          to="/login"
-        >
-          Login
-        </Link>
-      )}{" "}
-      {/*si no existe usuario muestra el link de login*/}
-      {!user && (
-        <Link
-          className={location.pathname === "/register" ? "active" : ""}
-          to="/register"
-        >
-          Register
-        </Link>
-      )}{" "}
-      {/*si no existe usuario muestra el link de register*/}
-      {user && (
-        <Link
-          className={location.pathname === "/start" ? "active" : ""}
-          to="/start"
-        >
-          Inicio
-        </Link>
-      )}
-      {user && (
-        <Link
-          className={location.pathname === "/spaces" ? "active" : ""}
-          to="/spaces"
-        >
-          Espacios
-        </Link>
-      )}
-      {user && (
-        <Link
-          className={location.pathname === "/devices" ? "active" : ""}
-          to="/devices"
-        >
-          Dispositivos
-        </Link>
-      )}
-      {user && (
-        <Link
-          className={location.pathname === "/settings" ? "active" : ""}
-          to="/settings"
-        >
-          Ajustes
-        </Link>
-      )}
-      {user && (
-        <Link
-          className={location.pathname === "/my-account" ? "active" : ""}
-          to="/my-account"
-        >
-          Mi Cuenta
-        </Link>
-      )}
-      {user && (
-        <button className="logout" onClick={() => dispatch(logoutUser())}>
-          Logout
-        </button>
-      )}{" "}
-      {/*si existe usuario muestra el botton de logout*/}
-      {user && <h3>Bienvenido {user.name}</h3>}
-    </div>
-  ));
+    )
+  );
 };
 
 export default Navbar;
