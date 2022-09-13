206cfc167d51931ee6d833b7bcbd83e30aa5786a
Jammings <rl.93@hotmail.com>
2022-09-13 13:09:01 +0200

    adding img to space 50%

--- sicurezza-front/src/pages/Spaces.jsx
+++ sicurezza-front/src/pages/Spaces.jsx
@@ -1,17 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Form from "./Form";
+import image1 from "../assets/greenUsers.svg";
+import axios from "axios";
+
+
 
 
 
 function Spaces() {
   const user = useSelector(state => state.auth.user);
-    
+  const [devices, setDevices] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get("http://localhost:4500/products")
+      .then((response) => setDevices(response.data))
+      .catch((error) => console.error(error))
+  }, [])
   return (
     <>
       <div>
         <Form/>
-        {user.room && user.room.map((room) => { return(<div>{room.name}</div>)})}
+        {user.room && user.room.map((room) => { return(<div>{room.name}
+        {devices.map((device) => (<option value={device._id}><img width='50px' src={device.img} alt=""></img></option>))}
+                    
+                    
+        </div>)})}
         {console.log(user.room)}
       </div>
     </>
