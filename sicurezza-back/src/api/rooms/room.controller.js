998a28658a307af09ee96c1159cea0717c307a2c
Jammings <rl.93@hotmail.com>
2022-09-13 15:10:55 +0200

    resolve refresh errors

--- sicurezza-back/src/api/rooms/room.controller.js
+++ sicurezza-back/src/api/rooms/room.controller.js
@@ -2,7 +2,7 @@ const Room = require('./room.model');
 
 const getAllRooms = async (req, res, next) => {
     try{
-        const allRooms = await Room.find().populate('products');
+        const allRooms = await Room.find().populate('product');
         return res.status(200).json(allRooms);
     } catch(error){
         return next(error);
@@ -12,8 +12,7 @@ const getAllRooms = async (req, res, next) => {
 const getRoom = async (req,res,next) =>{
     try{
         const {id} = req.params;//Forma de recoger la id con destructuring
-        const room = await Room.findById(id).populate('products');//Como la linea 7 de ciema model
-        const img = await Room.findById(img).populate('products');//Como la linea 7 de ciema model
+        const room = await Room.findById(id).populate('product');//Como la linea 7 de ciema model
         if(room) return res.status(200).json(room);
         else return res.status(404).json('Room no encontrado');
     }catch(error){
--- sicurezza-front/src/pages/Spaces.jsx
+++ sicurezza-front/src/pages/Spaces.jsx
@@ -1,36 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Form from "./Form";
-import image1 from "../assets/greenUsers.svg";
 import axios from "axios";
 
-
-
-
-
 function Spaces() {
-  const user = useSelector(state => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
   const [devices, setDevices] = useState([]);
-  
+
   useEffect(() => {
     axios
       .get("http://localhost:4500/products")
       .then((response) => setDevices(response.data))
-      .catch((error) => console.error(error))
-  }, [])
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <>
       <div>
-        <Form/>
-        {user.room && user.room.map((room) => { return(<div>{room.name}
-        {devices.map((device) => (<option value={device._id}><img width='50px' src={device.img} alt=""></img></option>))}
-                    
-                    
-        </div>)})}
-        {console.log(user.room)}
+        {console.log(user)}
+        <Form />
+        {user && user.room && user.room.length > 0 &&
+          user.room.map((room) => {
+            return (
+              <div className="products-room">
+                {room && room.name}
+                {room.product && room.product.length > 0 && room.product.map((product) => (
+                  <img width="150px" src={product.img} alt=""></img>
+                ))}
+              </div>
+            );
+          })}
       </div>
     </>
-      
   );
 }
 
